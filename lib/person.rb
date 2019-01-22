23f8cf44d82e433a60d853104ff4c6b576512645
Jared Harbison <jared.harbison@gmail.com>
2019-01-22 20:38:20 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  
+  def name=(persons_name)
+    @name = persons_name
+  end
+  def name
+    @name 
+  end
+  
+  def job=(job)
+    @breed = job
+  end
+  def job
+    @breed
+  end 
+  
+end
